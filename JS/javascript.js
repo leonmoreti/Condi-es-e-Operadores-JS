8db2e02e5235cd114cf91747dfdb4f885fd46eba
leonmoreti <leonmoreti7@gmail.com>
2022-10-20 15:33:53 -0300

    first commit

---
+++ JS/javascript.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+
+    // == igual
+    // >= menor que ou igual
+    // <= maior que ou igual
+    // > maior que
+    // < menor que
+
+    var idade = 2016-1993;
+
+    if(idade > 23){
+        console.log("Eu tenho mais de 23 anos!")
+    }else if(idade < 10){
+        console.log("Menor que 10");
+    }else if(idade < 5){
+        console.log("Menor que 5");
+    }else{
+        console.log("Nenhuma das condições são verdadeiras.");
+        }
+        
+}
